a546cffef8099bfd670460f0c2a697c966f96e99
Yadagini-Charishma <116977111+Yadagini-Charishma@users.noreply.github.com>
2023-10-11 00:10:52 +0530

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  // Define a state variable 'count' with an initial value of 0
+  const [count, setCount] = useState(0);
+
+  // Function to increment the count
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Function to decrement the count
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="counter">
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
